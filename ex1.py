41d3d56f18ecb82b156b8622298ec2026def8f71
VikKab7 <kabakova.va@phystech.edu>
2025-03-07 13:01:03 +0300

    ex2.py

--- ex1.py
+++ ex1.py
@@ -1,7 +1,24 @@
-def ad_matrix(N, edge_list):
-    matrix = [[0] * N for _ in range(N)]
-    for x, y in edge_list:
-        matrix[x][y] = 1
-        matrix[y][x] = 1
+def adj_matrix(N, edge_list):
+    res = [[0 for j in range(N)] for i in range(N)]
 
-    return matrix
+    for x, y in edge_list:
+        res[x][y] = 1
+        res[y][x] = 1
+
+    return res
+
+def print_matrix(matrix):
+  for line in matrix:
+    print(*line)
+
+
+N = int(input())
+M = int(input())
+edge_list = []
+for _ in range(M):
+    u, v = map(int, input().split())
+    edge_list.append((u, v))
+
+
+ad_matrix = adj_matrix(N, edge_list)
+print_matrix(ad_matrix)
